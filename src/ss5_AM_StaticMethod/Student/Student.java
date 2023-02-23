18d2f67bde4f6adaa1fffe551229589c4dda5d84
tienhung79 <ngotienhung2000@gmail.com>
2023-02-23 10:28:48 +0700

    nopbai

---
+++ src/ss5_AM_StaticMethod/Student/Student.java
@@ -0,0 +1,34 @@
+package ss5_AM_StaticMethod.Student;
+
+public class Student {
+    private String name = "John";
+    private String classes = "C02";
+
+    Student(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", classes='" + classes + '\'' +
+                '}';
+    }
+}
---
+++ src/ss5_AM_StaticMethod/Student/Test.java
@@ -0,0 +1,10 @@
+package ss5_AM_StaticMethod.Student;
+
+public class Test {
+    public static void main(String[] args) {
+        Student Test = new Student();
+        Test.setName("hung");
+        Test.setClasses("C12");
+        System.out.println(Test.toString());
+    }
+}
