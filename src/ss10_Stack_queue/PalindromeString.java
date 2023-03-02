35c8fb0973d2dc3f2e1f1fc8c14555cf27c24163
tienhung79 <ngotienhung2000@gmail.com>
2023-03-02 11:26:49 +0700

    nopbai

--- src/ss10_Stack_queue/PalindromeString.java
+++ src/ss10_Stack_queue/PalindromeString.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class CountElement {
+public class PalindromeString {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
