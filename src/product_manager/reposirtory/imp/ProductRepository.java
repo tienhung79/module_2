a4699325ecf07d962297ea6bd46d93c19c009cd5
tienhung79 <ngotienhung2000@gmail.com>
2023-03-07 20:21:53 +0700

    nopbai

--- src/product_manager/reposirtory/imp/ProductRepository.java
+++ src/product_manager/reposirtory/imp/ProductRepository.java
@@ -7,22 +7,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductRepository implements IProductReopository {
-    Produce produce =new Produce();
-    public static List<Produce> produceList= new ArrayList<>();
+    Produce produce = new Produce();
+    public static List<Produce> produceList = new ArrayList<>();
+
     static {
-        produceList.add(new Produce(123,"xe",34000));
-        produceList.add(new Produce(456,"xe dap",26000));
-        produceList.add(new Produce(789,"xe may",89000));
-        produceList.add(new Produce(789,"o to",89000));
+        produceList.add(new Produce(123, "xe", 34000));
+        produceList.add(new Produce(456, "xe dap", 26000));
+        produceList.add(new Produce(789, "xe may", 89000));
+        produceList.add(new Produce(789, "o to", 89000));
     }
-
-
-
 
     @Override
     public void add(Produce produce) {
         produceList.add(produce);
     }
+
     @Override
     public void delete(int i) {
         produceList.remove(i);
@@ -34,13 +33,13 @@ public class ProductRepository implements IProductReopository {
     }
 
     @Override
-    public void repairn(int i ,Produce produce) {
-        produceList.set(i,produce);
+    public void repairn(int i, Produce produce) {
+        produceList.set(i, produce);
     }
 
     @Override
     public void disPlayProduct(int i) {
-        System.out.println( produceList.get(i).getNameProduct());
+        System.out.println(produceList.get(i).getNameProduct());
     }
 
     @Override
